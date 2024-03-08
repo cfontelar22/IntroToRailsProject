cf0ce6922594a4002e56232847a5869bd4556a9b
cfontelar <fontelarcj@gmail.com>
2024-03-07 23:22:41 -0600

    Apply migration for Borrowing model to setup foreign keys for User-Book associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_08_050844) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_08_052007) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -20,6 +20,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_08_050844) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["library_branch_id"], name: "index_books_on_library_branch_id"
+  end
+
+  create_table "borrowings", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "book_id", null: false
+    t.date "due_date"
+    t.date "returned_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_borrowings_on_book_id"
+    t.index ["user_id"], name: "index_borrowings_on_user_id"
   end
 
   create_table "library_branches", force: :cascade do |t|
@@ -47,4 +58,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_08_050844) do
   end
 
   add_foreign_key "books", "library_branches"
+  add_foreign_key "borrowings", "books"
+  add_foreign_key "borrowings", "users"
 end
