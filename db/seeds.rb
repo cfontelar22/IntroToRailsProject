59ad25fcd983d567890d8a7d34b561c16dc0a3b3
cfontelar <fontelarcj@gmail.com>
2024-03-12 21:37:56 -0500

    Add seed data for users, weather, and library data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,51 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# db/seeds.rb
+
+require 'faker'
+require 'httparty'
+require 'csv'
+
+# Clear existing data
+User.delete_all
+Book.delete_all
+LibraryBranch.delete_all
+Weather.delete_all
+
+# Faker for Users
+100.times do
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email
+  )
+end
+
+# OpenWeatherMap API for Weather
+cities = ['New York', 'Los Angeles', 'Chicago']
+cities.each do |city|
+  response = HTTParty.get("https://api.openweathermap.org/data/2.5/weather?q=#{city}&appid=#{ENV['OPENWEATHERMAP_API_KEY']}&units=metric")
+  if response.success?
+    data = response.parsed_response
+    Weather.create!(
+      location: data['name'],
+      temperature: data['main']['temp'],
+      humidity: data['main']['humidity'],
+      # Include other attributes as needed
+    )
+  else
+    puts "Error fetching weather data for #{city}"
+  end
+end
+
+# CSV for Library Data
+CSV.foreach(Rails.root.join('lib/seeds/library_data.csv'), headers: true) do |row|
+  library_branch = LibraryBranch.find_or_create_by!(name: row['Library Name'], location: row['Library Location'])
+  Book.create!(
+    title: row['Title'],
+    author: row['Author'],
+    genre: row['Genre'],
+    publication_year: row['Publication Year'],
+    library_branch: library_branch
+  )
+end
+
+puts "Database seeded successfully!"
+cd ..
