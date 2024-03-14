1662e327180448f8807d9ac8eb60a165944a8a69
cfontelar <fontelarcj@gmail.com>
2024-03-14 13:32:56 -0500

    Edited the book views by 10 per page.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,10 @@
 class BooksController < ApplicationController
-    def index
-      @books = Book.all
-    end
-
-    def show
-        @book = Book.find(params[:id])
-    end
-      
+  def index
+    @books = Book.page(params[:page]).per(10) # Adjust the number (10) as needed
   end
   
+
+  def show
+    @book = Book.find(params[:id]) 
+  end
+end
